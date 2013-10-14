edef84e5fe9bc3e1027727fc3b688e13ecf8c214
bonillus_maximus <jarodriguezbonilla@gmail.com>
2013-10-14 20:54:52 +0200

    iguales

--- pepa.php
+++ pepa.php
@@ -1,5 +1,13 @@
 <?php
 
-echo 'hola';
+function test_one() {
 
+  for($x=1;$x<=10;$x++) {
+    echo 'Yo soy tu bucle ...';
+  }
+
+  for($y=5;$y<=20;$y++) {
+    echo 'Soy otro bucle en el desierto';
+  }
+}
 ?>
